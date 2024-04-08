14ed8cafb8312f44fcaede3ad839e7440c0f7c0f
Jeffrey Podmayer <jeff.podmayer@gmail.com>
2024-04-08 12:50:53 -0700

    edited landing page

--- src/main/java/com/coderscampus/hikerhunger/web/UserController.java
+++ src/main/java/com/coderscampus/hikerhunger/web/UserController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
-    @GetMapping("")
+    @GetMapping("/landingPage")
     public String getLandingPage(){
         return "landingPage";
     }

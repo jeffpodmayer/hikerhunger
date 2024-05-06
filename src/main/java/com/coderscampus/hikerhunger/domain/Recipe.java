996118ac512f02e5a622a88a3650a59580bea9be
Jeffrey Podmayer <jeff.podmayer@gmail.com>
2024-05-06 14:57:29 -0700

    Update Recipe.java

--- src/main/java/com/coderscampus/hikerhunger/domain/Recipe.java
+++ src/main/java/com/coderscampus/hikerhunger/domain/Recipe.java
@@ -24,39 +24,7 @@ public class Recipe {
 
     @OneToMany(mappedBy = "recipe")
     private List<TripRecipe> tripRecipes = new ArrayList<>();
-
-//    public Recipe createCopy() {
-//        Recipe copy = new Recipe();
-//        copy.setRecipeId(this.recipeId);
-//        copy.setUser(this.user);
-//        copy.setRecipeName(this.recipeName);
-//        copy.setRecipeType(this.recipeType);
-//        copy.setInstructions(this.instructions);
-//        copy.setServings(this.servings);
-//        copy.setTotalWeight(this.totalWeight);
-//        copy.setTrips(this.trips);
-//
-//
-//        List<Ingredient> ingredientCopies = new ArrayList<>();
-//        for (Ingredient ingredient : this.ingredients) {
-//            Ingredient ingredientCopy = new Ingredient();
-//            ingredientCopy.setIngredientId(ingredient.getIngredientId());
-//            ingredientCopy.setRecipe(ingredient.getRecipe());
-//            ingredientCopy.setIngredientName(ingredient.getIngredientName());
-//            ingredientCopy.setQuantity(ingredient.getQuantity());
-//            ingredientCopy.setUnit(ingredientCopy.getUnit());
-//            ingredientCopy.setWeightInGrams(ingredient.getWeightInGrams());
-//            ingredientCopy.setNotes(ingredient.getNotes());
-//
-//            ingredientCopies.add(ingredientCopy);
-//        }
-//        copy.setIngredients(ingredientCopies);
-//
-//        return copy;
-//    }
-
-
-
+    
     public enum RecipeType {
         BREAKFAST("Breakfast"),
         LUNCH("Lunch"),
